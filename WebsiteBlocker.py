648d90e2bca6cc99e1db86d89b1c316961d1cc92
Hitesh <hiteshkumarpattanatyak03@gmail.com>
2020-02-08 02:09:16 +0530

    first draft

---
+++ WebsiteBlocker.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 22:58:14 2020
+
+@author: Hitesh Pattanayak
+"""
+import time
+from datetime import datetime as dt
+
+hosts_temp = "hosts"
+hosts_path = 'C:\\Windows\\System32\\drivers\\etc\\hosts' 
+redirect = "127.0.0.1"
+website_list = ['www.apple.com', 'vimeo.com', 'www.vimeo.com']
+
+while True:
+    
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print('Working hours')
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print('Fun hours')
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+    
+    time.sleep(5)
